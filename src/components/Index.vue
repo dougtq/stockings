9251f62a427f59158e38a756ab56bf2959f1f470
Douglas Alves <douglas.expalves@gmail.com>
2018-04-07 04:14:29 -0300

    update carousel interval

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,6 +1,6 @@
 <template>
   <article>
-    <v-carousel interval="3000" hide-delimiters hide-controls dark>
+    <v-carousel interval="6000" hide-delimiters light>
       <v-carousel-item  v-for="(item, i) in pics" :src="item.src" :key="i"></v-carousel-item>
     </v-carousel>
   </article>
@@ -11,12 +11,13 @@ export default {
   name: 'Index',
   data () {
     return {
-      pics: [{
-        src: 'http://gastown.org/wp-content/uploads/2015/03/Neighbour-5.jpg'
-      },
-      {
-        src: 'http://futurelankan.com/wp-content/uploads/2017/06/bridge-and-burn-clothing-store-1.jpg'
-      }
+      pics: [
+        {
+          src: 'http://gastown.org/wp-content/uploads/2015/03/Neighbour-5.jpg'
+        },
+        {
+          src: 'http://futurelankan.com/wp-content/uploads/2017/06/bridge-and-burn-clothing-store-1.jpg'
+        }
       ]
     }
   }
